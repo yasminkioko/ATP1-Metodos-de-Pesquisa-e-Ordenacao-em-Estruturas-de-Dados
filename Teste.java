0ad178200ac7631a62186850a9f213e958915a24
Yasmin Kioko <52516478+yasminkioko@users.noreply.github.com>
2023-10-20 13:11:38 -0300

    Teste.java

--- Teste.java
+++ Teste.java
@@ -6,31 +6,31 @@ public class Teste {
         Lista lista = new Lista();
 
 
-        //lista.Mostrar();
-        //System.out.println(lista.getPrimeiro());
+        lista.Mostrar();
+        System.out.println(lista.getPrimeiro());
 
-        //lista.inserePrimeiro(10);
-        //lista.inserePrimeiro(20);
-        //lista.inserePrimeiro(30);
-        //lista.inserePrimeiro(40);
-        //lista.Mostrar();
+        lista.inserePrimeiro(10);
+        lista.inserePrimeiro(20);
+        lista.inserePrimeiro(30);
+        lista.inserePrimeiro(40);
+        lista.Mostrar();
 
-        //lista.insereUltimo(05);
-        //lista.Mostrar();
+        lista.insereUltimo(05);
+        lista.Mostrar();
 
-        //No no1 = lista.getPrimeiro();
-        //lista.insereDepois(no1, 25);
-        //lista.Mostrar();
+        No no1 = lista.getPrimeiro();
+        lista.insereDepois(no1, 25);
+        lista.Mostrar();
         
         lista.removePrimeiro();
-        //lista.Mostrar();
+        lista.Mostrar();
 
-        //lista.removeUltimo();
-        //lista.Mostrar();
+        lista.removeUltimo();
+        lista.Mostrar();
 
 
-        //lista.removeDepois(lista.getPrimeiro().getProximo());
-        //lista.Mostrar();
+        lista.removeDepois(lista.getPrimeiro().getProximo());
+        lista.Mostrar();
 
     }
 }
