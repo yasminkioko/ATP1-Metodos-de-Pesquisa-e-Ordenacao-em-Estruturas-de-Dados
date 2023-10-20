58fff3feb3ab0921f3f2f42265b1d36788033cd2
Yasmin Kioko <52516478+yasminkioko@users.noreply.github.com>
2023-10-20 13:12:23 -0300

    No.java

--- No.java
+++ No.java
@@ -4,24 +4,24 @@ public class No {
     private int elemento;
     private No proximo;
 
-    public No(int elemento) {//primeiro nó
-        this.elemento = elemento;// nó recebe elemento inteiro
-        this.proximo = null; //não há proximo
+    public No(int elemento) {
+        this.elemento = elemento;
+        this.proximo = null; 
     }
 
-    public int getElemento() { //qual o elemento?
+    public int getElemento() { 
         return elemento;
     }
 
-    public void setElemento(int elemento) { //void não retorna
-        this.elemento = elemento; //this.elemento refere a private int elemento; elemento refere a int elemento
+    public void setElemento(int elemento) { 
+        this.elemento = elemento; 
     }
 
-    public No getProximo() { //qual o elemento do proximo?
+    public No getProximo() { 
         return proximo;
     }
 
-    public void setProximo(No proximo) { //define o proximo nó
+    public void setProximo(No proximo) { 
         this.proximo = proximo;
     }
 
